f5b75d25c3697e8d0bb25dc48025ff550523de60
Rafael Faita <rafael.faita.ext@nokia.com>
2020-01-03 17:22:31 +0000

    fix influxdb port

--- src/main/java/com/iot/edge/config/StreamConfig.java
+++ src/main/java/com/iot/edge/config/StreamConfig.java
@@ -2,7 +2,6 @@ package com.iot.edge.config;
 
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Processor;
-import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
